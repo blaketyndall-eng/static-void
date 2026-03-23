0029c1dd4562e2168d6de5fddba2229aeb8cbf7c
blaketyndall-eng <blaketyndall@gmail.com>
2026-03-23 18:10:17 -0500

    Compose fuller router-based packaged product app

---
+++ app_product_router_full.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from packages.routers.product_combined_views import router as combined_views_router
+from packages.routers.product_decision_board import router as decision_board_router
+from packages.routers.product_evaluation_views import router as evaluation_views_router
+from packages.routers.product_recommendations import router as recommendation_views_router
+
+app = FastAPI(title="Full Router-Based Packaged Product Surface API")
+app.include_router(decision_board_router)
+app.include_router(evaluation_views_router)
+app.include_router(recommendation_views_router)
+app.include_router(combined_views_router)
