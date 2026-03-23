1856c255b66a70c896cf5ab04b550302f28c293b
blaketyndall-eng <blaketyndall@gmail.com>
2026-03-23 12:16:46 -0500

    Add service container and shared stores

---
+++ service_container.py
@@ -0,0 +1,8 @@
+from evaluation_service import EvaluationService
+from opportunity_service import OpportunityService
+from source_registry import SourceRegistry
+
+
+source_registry = SourceRegistry()
+opportunity_service = OpportunityService()
+evaluation_service = EvaluationService()
